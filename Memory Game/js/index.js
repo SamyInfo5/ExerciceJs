c278a2d0992a5491bd9d936733a6a40732a8e2f8
Samuel Dhondt <samueldhondt@MacBook-Air-de-Samuel.local>
2024-02-19 09:05:11 +0100

    realisation du jeux de memoire

--- Memory Game/js/index.js
+++ Memory Game/js/index.js
@@ -1,26 +1,50 @@
 const keyApi = "31U1bqlQs6hNTPvgY7zeXzS4KN2ZT0HQCgrlfr40nypNqgqY3q1XE2eW";
-const BASE_URL = "https://api.pexels.com/v1/";
+const BASE_URL = "https://api.pexels.com/v1/search";
 const box_Game = document.getElementById("game");
+let flippedCards = 0;
 let tab;
 let tab2;
+let tabCheck = [];
+let tabChild = [];
 
 const shuffle = (arr) => {
   arr.sort(() => Math.random() - 0.5);
 };
 
-async function fetchData(url, key) {
-  const res = await fetch(BASE_URL + url, {
+const random = () => {
+  return Math.floor(Math.random() * 1000);
+};
+
+const params = {
+  query : 'people',
+  page: random(),
+  per_page: 6
+}
+
+const encodedParams = Object.keys(params).map(key => {
+  if (Array.isArray(params[key])) {
+    return params[key].map(value => `${encodeURIComponent(key)}[]=${encodeURIComponent(value)}`).join('&');
+  }
+  return `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`;
+});
+
+const queryString = encodedParams.join('&');
+
+const url = `${BASE_URL}?${queryString}`
+
+async function fetchData(url) {
+  const res = await fetch(url, {
     method: "GET",
     mode: "cors",
     headers: {
-      Authorization: key,
+      Authorization: keyApi,
       "Content-Type": "application/json",
     },
   });
   return res.json();
 }
 
-fetchData("search?query=people&per_page=6", keyApi)
+fetchData(url, keyApi)
   .then((data) => {
     tab = data.photos;
     tab2 = data.photos;
@@ -29,31 +53,87 @@ fetchData("search?query=people&per_page=6", keyApi)
     });
     shuffle(tab);
     tab.forEach((item) => {
-      console.log("item", item);
       const flipCard = document.createElement("div");
       flipCard.classList.add("flip-card");
       const flipCardInner = document.createElement("div");
       flipCardInner.classList.add("flip-card-inner");
+      flipCardInner.setAttribute("id", item.id);
       const flipCardFront = document.createElement("div");
       flipCardFront.classList.add("flip-card-front");
       const imgFront = document.createElement("img");
-      imgFront.src  = './assets/front.png'
-      imgFront.style.height = '130px'
+      imgFront.src = "./assets/front.png";
+      imgFront.style.height = "13vh";
       const flipCardBack = document.createElement("div");
-      flipCardBack
       const imgBack = document.createElement("img");
-      imgBack.src = item.src.tiny
-      imgBack.style.height = '100px';
+      imgBack.classList.add("backImg");
+      imgBack.src = item.src.tiny;
+      imgBack.style.height = "10vh";
 
-      flipCard.appendChild(flipCardInner)
-      flipCardInner.appendChild(flipCardFront)
-      flipCardInner.appendChild(flipCardBack)
-      flipCardFront.appendChild(imgFront)
-      flipCardBack.appendChild(imgBack)
-      box_Game.appendChild(flipCard)
-
+      flipCard.appendChild(flipCardInner);
+      flipCardInner.appendChild(flipCardFront);
+      flipCardInner.appendChild(flipCardBack);
+      flipCardFront.appendChild(imgFront);
+      flipCardBack.appendChild(imgBack);
+      box_Game.appendChild(flipCard);
     });
+    selectCard();
   })
   .catch((error) => {
     console.error("Error:", error);
   });
+
+const selectCard = () => {
+  const card = document.querySelectorAll(".flip-card .flip-card-inner");
+  card.forEach((item) => {
+    item.addEventListener("click", () => {
+      if (!item.classList.contains("flip") && !tabCheck.includes(item) && flippedCards < 2) {
+        item.style.transform = "rotateY(180deg)";
+        item.classList.add("flip");
+        tabCheck.push(item);
+        flippedCards++;
+        checkCardClicked();
+      }
+    });
+  });
+};
+
+const checkCardClicked = () => {
+  if (tabCheck.length == 2) {
+    let allcardflip = document.querySelectorAll(".flip");
+    if (tabCheck[0]["id"] == tabCheck[1]["id"]) {
+      console.log("coucou");
+      setTimeout(() => {
+        validateFlip(allcardflip);
+        tabCheck = [];
+        flippedCards = 0
+      }, 500)
+    } else {
+      console.log("rater");
+      setTimeout(() => {
+        removeClassFlip(allcardflip);
+        tabCheck = [];
+        flippedCards = 0
+      }, 1000);
+    }
+  }
+};
+
+const removeClassFlip = (element) => {
+  element.forEach((item) => {
+    item.classList.remove("flip");
+    item.style.transform = "rotateY(0deg)";
+  });
+};
+
+const validateFlip = (element) => {
+  element.forEach((item) => {
+    item.style.opacity = "0";
+  });
+};
+
+
+const endGame = document.getElementById('endGame')
+
+const  allCardReveal = () => {
+
+}
