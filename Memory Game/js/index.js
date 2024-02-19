1babf8265300a79ecc071908afe442358989f6b7
Samuel Dhondt <samueldhondt@MacBook-Air-de-Samuel.local>
2024-02-19 10:09:59 +0100

    add begin/end game

--- Memory Game/js/index.js
+++ Memory Game/js/index.js
@@ -1,11 +1,53 @@
 const keyApi = "31U1bqlQs6hNTPvgY7zeXzS4KN2ZT0HQCgrlfr40nypNqgqY3q1XE2eW";
 const BASE_URL = "https://api.pexels.com/v1/search";
 const box_Game = document.getElementById("game");
+const play = document.getElementById("playGame");
 let flippedCards = 0;
 let tab;
 let tab2;
 let tabCheck = [];
 let tabChild = [];
+let cardValidate = 0;
+
+play.addEventListener("click", () => {
+  fetchData(url, keyApi)
+    .then((data) => {
+      tab = data.photos;
+      tab2 = data.photos;
+      tab2.forEach((element) => {
+        tab.push(element);
+      });
+      shuffle(tab);
+      tab.forEach((item) => {
+        const flipCard = document.createElement("div");
+        flipCard.classList.add("flip-card");
+        const flipCardInner = document.createElement("div");
+        flipCardInner.classList.add("flip-card-inner");
+        flipCardInner.setAttribute("id", item.id);
+        const flipCardFront = document.createElement("div");
+        flipCardFront.classList.add("flip-card-front");
+        const imgFront = document.createElement("img");
+        imgFront.src = "./assets/front.png";
+        imgFront.style.height = "13vh";
+        const flipCardBack = document.createElement("div");
+        const imgBack = document.createElement("img");
+        imgBack.classList.add("backImg");
+        imgBack.src = item.src.tiny;
+        imgBack.style.height = "10vh";
+
+        flipCard.appendChild(flipCardInner);
+        flipCardInner.appendChild(flipCardFront);
+        flipCardInner.appendChild(flipCardBack);
+        flipCardFront.appendChild(imgFront);
+        flipCardBack.appendChild(imgBack);
+        box_Game.appendChild(flipCard);
+      });
+      selectCard();
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+});
 
 const shuffle = (arr) => {
   arr.sort(() => Math.random() - 0.5);
@@ -16,21 +58,25 @@ const random = () => {
 };
 
 const params = {
-  query : 'people',
+  query: "people",
   page: random(),
-  per_page: 6
-}
+  per_page: 6,
+};
 
-const encodedParams = Object.keys(params).map(key => {
+const encodedParams = Object.keys(params).map((key) => {
   if (Array.isArray(params[key])) {
-    return params[key].map(value => `${encodeURIComponent(key)}[]=${encodeURIComponent(value)}`).join('&');
+    return params[key]
+      .map(
+        (value) => `${encodeURIComponent(key)}[]=${encodeURIComponent(value)}`
+      )
+      .join("&");
   }
   return `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`;
 });
 
-const queryString = encodedParams.join('&');
+const queryString = encodedParams.join("&");
 
-const url = `${BASE_URL}?${queryString}`
+const url = `${BASE_URL}?${queryString}`;
 
 async function fetchData(url) {
   const res = await fetch(url, {
@@ -44,49 +90,15 @@ async function fetchData(url) {
   return res.json();
 }
 
-fetchData(url, keyApi)
-  .then((data) => {
-    tab = data.photos;
-    tab2 = data.photos;
-    tab2.forEach((element) => {
-      tab.push(element);
-    });
-    shuffle(tab);
-    tab.forEach((item) => {
-      const flipCard = document.createElement("div");
-      flipCard.classList.add("flip-card");
-      const flipCardInner = document.createElement("div");
-      flipCardInner.classList.add("flip-card-inner");
-      flipCardInner.setAttribute("id", item.id);
-      const flipCardFront = document.createElement("div");
-      flipCardFront.classList.add("flip-card-front");
-      const imgFront = document.createElement("img");
-      imgFront.src = "./assets/front.png";
-      imgFront.style.height = "13vh";
-      const flipCardBack = document.createElement("div");
-      const imgBack = document.createElement("img");
-      imgBack.classList.add("backImg");
-      imgBack.src = item.src.tiny;
-      imgBack.style.height = "10vh";
-
-      flipCard.appendChild(flipCardInner);
-      flipCardInner.appendChild(flipCardFront);
-      flipCardInner.appendChild(flipCardBack);
-      flipCardFront.appendChild(imgFront);
-      flipCardBack.appendChild(imgBack);
-      box_Game.appendChild(flipCard);
-    });
-    selectCard();
-  })
-  .catch((error) => {
-    console.error("Error:", error);
-  });
-
 const selectCard = () => {
   const card = document.querySelectorAll(".flip-card .flip-card-inner");
   card.forEach((item) => {
     item.addEventListener("click", () => {
-      if (!item.classList.contains("flip") && !tabCheck.includes(item) && flippedCards < 2) {
+      if (
+        !item.classList.contains("flip") &&
+        !tabCheck.includes(item) &&
+        flippedCards < 2
+      ) {
         item.style.transform = "rotateY(180deg)";
         item.classList.add("flip");
         tabCheck.push(item);
@@ -101,18 +113,18 @@ const checkCardClicked = () => {
   if (tabCheck.length == 2) {
     let allcardflip = document.querySelectorAll(".flip");
     if (tabCheck[0]["id"] == tabCheck[1]["id"]) {
-      console.log("coucou");
       setTimeout(() => {
         validateFlip(allcardflip);
         tabCheck = [];
-        flippedCards = 0
-      }, 500)
+        cardValidate += 1;
+        flippedCards = 0;
+        allCardReveal();
+      }, 500);
     } else {
-      console.log("rater");
       setTimeout(() => {
         removeClassFlip(allcardflip);
         tabCheck = [];
-        flippedCards = 0
+        flippedCards = 0;
       }, 1000);
     }
   }
@@ -131,9 +143,15 @@ const validateFlip = (element) => {
   });
 };
 
+const endGame = document.getElementById("endGame");
+const replay = document.querySelector(".endGame-Button");
 
-const endGame = document.getElementById('endGame')
+const allCardReveal = () => {
+  if (cardValidate == params.per_page) {
+    endGame.style.display = "flex";
+  }
+};
 
-const  allCardReveal = () => {
-
-}
+replay.addEventListener("click", () => {
+  
+})
